f28068d6af5e8c49d75b58812c869d2d76b7b6c7
Myuse <myuse@gmail.com>
2026-07-12 10:04:15 +0530

    new

--- Function/function.py
+++ Function/function.py
@@ -213,13 +213,10 @@
 # # 10.Write a function to check whether a number is prime.
 # def prime(n):
 #     count = 0
-
 #     for i in range(1, n + 1):
 #         if n % i == 0:
 #             count += 1
-
 #     if count == 2:
 #         return "Prime"
 #     return "Not Prime"
-
 # print(prime(13))
