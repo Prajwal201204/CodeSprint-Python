d0e12b1fca51dab4b145a6c06453bc9843705511
Prajwal Nevase <prajwalnevase04@gmail.com>
2026-08-02 20:12:40 +0530

    Update ttime pass.py

--- ttime pass.py
+++ ttime pass.py
@@ -44,4 +44,18 @@
 # elif a>=60:
 #     print("d")
 # else:
-#     print('fail') 
+#     print('fail')
+
+
+# # #5 Grade system
+# a=int(input("enter value"))
+# if a>=90:
+#     print("a")
+# elif a>=80:
+#     print("b")
+# elif a>=70:
+#     print("c")
+# elif a>=60:
+#     print("d")
+# else:
+#     print('fail')
